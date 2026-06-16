6fbde6dbc646cf8ae381bea4bb934e9f4817b3ad
Chenx Dust <chenx_dust@outlook.com>
2026-06-16 23:56:24 +0800

    Remove Firebase
    (cherry picked from commit 88a336c98710e81b62e4cb8e9e448373780c75f7)

--- test/plugins/app_test.dart
+++ test/plugins/app_test.dart
@@ -12,35 +12,4 @@ void main() {
     TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
         .setMockMethodCallHandler(channel, null);
   });
-
-  test('reads previous execution crash state from Android', () async {
-    MethodCall? receivedCall;
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(channel, (call) async {
-          receivedCall = call;
-          return true;
-        });
-
-    final didCrash = await App().didCrashOnPreviousExecution();
-
-    expect(didCrash, isTrue);
-    expect(receivedCall, isNotNull);
-    expect(receivedCall!.method, 'didCrashOnPreviousExecution');
-  });
-
-  test('uses false when Android returns no crash state', () async {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(channel, (_) async => null);
-
-    expect(await App().didCrashOnPreviousExecution(), isFalse);
-  });
-
-  test('uses false when crash detection is unavailable', () async {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(channel, (_) async {
-          throw PlatformException(code: 'unavailable');
-        });
-
-    expect(await App().didCrashOnPreviousExecution(), isFalse);
-  });
 }
