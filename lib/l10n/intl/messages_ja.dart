09314a625680d403e44f98f5f93e13a48fd134e9
Chenx Dust <chenx_dust@outlook.com>
2026-06-18 00:36:03 +0800

    Improve log ui

--- lib/l10n/intl/messages_ja.dart
+++ lib/l10n/intl/messages_ja.dart
@@ -367,6 +367,7 @@ class MessageLookup extends MessageLookupByLibrary {
     "key": MessageLookupByLibrary.simpleMessage("キー"),
     "language": MessageLookupByLibrary.simpleMessage("言語"),
     "layout": MessageLookupByLibrary.simpleMessage("レイアウト"),
+    "level": MessageLookupByLibrary.simpleMessage("レベル"),
     "light": MessageLookupByLibrary.simpleMessage("ライト"),
     "list": MessageLookupByLibrary.simpleMessage("リスト"),
     "listen": MessageLookupByLibrary.simpleMessage("リスン"),
