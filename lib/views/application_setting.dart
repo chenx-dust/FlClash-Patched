b723b78b1b4cbd9020ff97756ec9c6351d195d1b
Chenx Dust <chenx_dust@outlook.com>
2026-06-17 20:50:03 +0800

    Optional traffic notification
    Fix Android traffic notification and translation

--- lib/views/application_setting.dart
+++ lib/views/application_setting.dart
@@ -48,6 +48,28 @@ class UsageItem extends ConsumerWidget {
   }
 }
 
+class NetworkSpeedNotificationItem extends ConsumerWidget {
+  const NetworkSpeedNotificationItem({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final appLocalizations = context.appLocalizations;
+    final networkSpeedNotification = ref.watch(
+      vpnSettingProvider.select((state) => state.networkSpeedNotification),
+    );
+    return ListItem.toggle(
+      title: Text(appLocalizations.networkSpeedNotification),
+      subtitle: Text(appLocalizations.networkSpeedNotificationDesc),
+      value: networkSpeedNotification,
+      onChanged: (bool value) async {
+        ref
+            .read(vpnSettingProvider.notifier)
+            .update((state) => state.copyWith(networkSpeedNotification: value));
+      },
+    );
+  }
+}
+
 class MinimizeItem extends ConsumerWidget {
   const MinimizeItem({super.key});
 
@@ -241,6 +263,7 @@ class ApplicationSettingView extends StatelessWidget {
       const OpenLogsItem(),
       const CloseConnectionsItem(),
       const UsageItem(),
+      if (system.isAndroid) const NetworkSpeedNotificationItem(),
       const AutoCheckUpdateItem(),
     ];
     return BaseScaffold(
