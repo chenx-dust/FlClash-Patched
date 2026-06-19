e197aa9951e5594f9c93ec6fb102a16a1db430e2
Chenx Dust <chenx_dust@outlook.com>
2026-06-19 12:16:32 +0800

    Support age key

--- lib/models/generated/core.g.dart
+++ lib/models/generated/core.g.dart
@@ -295,6 +295,8 @@ const _$ActionMethodEnumMap = {
   ActionMethod.crash: 'crash',
   ActionMethod.setupConfig: 'setupConfig',
   ActionMethod.deleteFile: 'deleteFile',
+  ActionMethod.generateAgeKeyPair: 'generateAgeKeyPair',
+  ActionMethod.convertAgeSecretKeyToPublicKey: 'convertAgeSecretKeyToPublicKey',
   ActionMethod.setState: 'setState',
   ActionMethod.startTun: 'startTun',
   ActionMethod.stopTun: 'stopTun',
