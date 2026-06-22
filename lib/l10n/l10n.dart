12e8bb9f315cd33d6c95c27a5bb36265462f433e
Chenx Dust <chenx_dust@outlook.com>
2026-06-22 15:53:32 +0800

    Mixed port can be zero to disable

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -2664,10 +2664,10 @@ class AppLocalizations {
     return Intl.message('TProxy port', name: 'tproxyPort', desc: '', args: []);
   }
 
-  /// `{label} must be between 1024 and 49151`
+  /// `{label} must be between 1024 and 49151, 0 to disable`
   String portTip(Object label) {
     return Intl.message(
-      '$label must be between 1024 and 49151',
+      '$label must be between 1024 and 49151, 0 to disable',
       name: 'portTip',
       desc: '',
       args: [label],
