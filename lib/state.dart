b4f6e69af8fa527140c592dc6a3bc33a912673e8
Chenx Dust <chenx_dust@outlook.com>
2026-06-25 15:04:36 +0800

    Change log level and focus to resume ticker

--- lib/state.dart
+++ lib/state.dart
@@ -71,7 +71,7 @@ class GlobalState {
   BuildContext get _context => navigatorKey.currentContext!;
 
   void handleBackground() async {
-    commonPrint.log('background');
+    commonPrint.log('background', logLevel: LogLevel.debug);
     if (isBackground.value) {
       return;
     }
@@ -81,7 +81,7 @@ class GlobalState {
   }
 
   void handleForeground() {
-    commonPrint.log('foreground');
+    commonPrint.log('foreground', logLevel: LogLevel.debug);
     foregroundTicker.resume();
     if (!isBackground.value) {
       return;
