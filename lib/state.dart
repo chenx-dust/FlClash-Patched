ec3da08c4945da14f541634e196d7436fafb26d7
Chenx Dust <chenx_dust@outlook.com>
2026-06-26 01:36:55 +0800

    Improve focus logics

--- lib/state.dart
+++ lib/state.dart
@@ -406,6 +406,7 @@ class GlobalState {
     )) {
       return;
     }
+    await WidgetsBinding.instance.endOfFrame;
     final isDisclaimerAccepted = await showDisclaimer();
     if (!isDisclaimerAccepted) {
       await container.read(systemActionProvider.notifier).handleExit();
