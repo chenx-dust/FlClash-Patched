0124f507e2687271c7face33bb4b28a20db0f2bf
Chenx Dust <chenx_dust@outlook.com>
2026-06-27 20:22:14 +0800

    Check mounted after async

--- lib/views/dashboard/dashboard.dart
+++ lib/views/dashboard/dashboard.dart
@@ -211,6 +211,9 @@ class _DashboardViewState extends ConsumerState<DashboardView> {
   Future<void> _handleUpdateIsEdit() async {
     if (_isEditNotifier.value == true) {
       await _handleSave();
+      if (!mounted) {
+        return;
+      }
     }
     _isEditNotifier.value = !_isEditNotifier.value;
   }
@@ -251,6 +254,9 @@ class _DashboardViewState extends ConsumerState<DashboardView> {
           .map((item) => item.widget),
     ];
     WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) {
+        return;
+      }
       _addedWidgetsNotifier.value = DashboardWidget.values
           .where(
             (item) =>
