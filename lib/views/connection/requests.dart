4fb4a7dc037befa58b2a70c93b062e0180f7a92b
Chenx Dust <chenx_dust@outlook.com>
2026-06-27 23:30:35 +0800

    New filter ui

--- lib/views/connection/requests.dart
+++ lib/views/connection/requests.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:super_sliver_list/super_sliver_list.dart';
 
+import 'filter.dart';
 import 'item.dart';
 
 class RequestsView extends ConsumerStatefulWidget {
@@ -23,6 +24,8 @@ class _RequestsViewState extends ConsumerState<RequestsView> {
     const TrackerInfosState(),
   );
   List<TrackerInfo> _requests = [];
+  TrackerInfoFilter _trackerFilter = const TrackerInfoFilter();
+  bool _showFilterBar = false;
   late final ScrollController _scrollController;
   bool _requestListening = false;
 
@@ -38,10 +41,24 @@ class _RequestsViewState extends ConsumerState<RequestsView> {
     );
   }
 
-  void _onKeywordsUpdate(List<String> keywords) {
-    _requestsStateNotifier.value = _requestsStateNotifier.value.copyWith(
-      keywords: keywords,
-    );
+  void _setTrackerFilter(TrackerInfoFilter filter) {
+    setState(() {
+      _trackerFilter = filter;
+      if (filter.isNotEmpty) {
+        _showFilterBar = true;
+      }
+    });
+  }
+
+  void _toggleFilterBar() {
+    setState(() {
+      if (_showFilterBar || _trackerFilter.isNotEmpty) {
+        _showFilterBar = false;
+        _trackerFilter = const TrackerInfoFilter();
+        return;
+      }
+      _showFilterBar = true;
+    });
   }
 
   @override
@@ -123,12 +140,18 @@ class _RequestsViewState extends ConsumerState<RequestsView> {
     final appLocalizations = context.appLocalizations;
     return CommonScaffold(
       title: appLocalizations.requests,
+      actions: [
+        TrackerInfoFilterButton(
+          visible: _showFilterBar,
+          filter: _trackerFilter,
+          onPressed: _toggleFilterBar,
+        ),
+      ],
       searchState: AppBarSearchState(
         onSearch: _onSearch,
         onRegexChange: _onRegexSearchChange,
         useRegex: _requestsStateNotifier.value.useRegex,
       ),
-      onKeywordsUpdate: _onKeywordsUpdate,
       floatingActionButton: ValueListenableBuilder(
         valueListenable: _requestsStateNotifier,
         builder: (_, state, _) {
@@ -153,52 +176,70 @@ class _RequestsViewState extends ConsumerState<RequestsView> {
       body: ValueListenableBuilder<TrackerInfosState>(
         valueListenable: _requestsStateNotifier,
         builder: (context, state, _) {
-          final requests = state.list;
-          if (requests.isEmpty) {
-            return NullStatus(
-              label: appLocalizations.nullTip(appLocalizations.requests),
-            );
-          }
-          final items = requests
-              .map<Widget>(
-                (trackerInfo) => TrackerInfoItem(
-                  key: Key(trackerInfo.id),
-                  trackerInfo: trackerInfo,
-                  onClickKeyword: (value) {
-                    context.commonScaffoldState?.addKeyword(value);
-                  },
-                  detailTitle: appLocalizations.details(
-                    appLocalizations.request,
+          final requests = state.list.withTrackerFilter(_trackerFilter);
+          final body = () {
+            if (requests.isEmpty) {
+              return Expanded(
+                child: NullStatus(
+                  label: appLocalizations.nullTip(appLocalizations.requests),
+                ),
+              );
+            }
+            final items = requests
+                .map<Widget>(
+                  (trackerInfo) => TrackerInfoItem(
+                    key: Key(trackerInfo.id),
+                    trackerInfo: trackerInfo,
+                    onClickFilter: (type, value) {
+                      _setTrackerFilter(_trackerFilter.add(type, value));
+                    },
+                    detailTitle: appLocalizations.details(
+                      appLocalizations.request,
+                    ),
+                  ),
+                )
+                .separated(const Divider(height: 0))
+                .toList();
+            return Expanded(
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: CommonScrollBar(
+                  trackVisibility: false,
+                  controller: _scrollController,
+                  child: ScrollToEndBox(
+                    controller: _scrollController,
+                    dataSource: requests,
+                    enable: state.autoScrollToEnd,
+                    onCancelToEnd: () {
+                      _requestsStateNotifier.value = _requestsStateNotifier
+                          .value
+                          .copyWith(autoScrollToEnd: false);
+                    },
+                    child: SuperListView.builder(
+                      reverse: true,
+                      shrinkWrap: true,
+                      physics: const NextClampingScrollPhysics(),
+                      controller: _scrollController,
+                      itemBuilder: (_, index) {
+                        return items[index];
+                      },
+                      itemCount: items.length,
+                    ),
                   ),
                 ),
-              )
-              .separated(const Divider(height: 0))
-              .toList();
-          return Align(
-            alignment: Alignment.topCenter,
-            child: CommonScrollBar(
-              trackVisibility: false,
-              controller: _scrollController,
-              child: ScrollToEndBox(
-                controller: _scrollController,
-                dataSource: requests,
-                enable: state.autoScrollToEnd,
-                onCancelToEnd: () {
-                  _requestsStateNotifier.value = _requestsStateNotifier.value
-                      .copyWith(autoScrollToEnd: false);
-                },
-                child: SuperListView.builder(
-                  reverse: true,
-                  shrinkWrap: true,
-                  physics: const NextClampingScrollPhysics(),
-                  controller: _scrollController,
-                  itemBuilder: (_, index) {
-                    return items[index];
-                  },
-                  itemCount: items.length,
-                ),
               ),
-            ),
+            );
+          }();
+          return Column(
+            children: [
+              TrackerInfoFilterBar(
+                visible: _showFilterBar,
+                trackerInfos: state.trackerInfos,
+                filter: _trackerFilter,
+                onChanged: _setTrackerFilter,
+              ),
+              body,
+            ],
           );
         },
       ),
