4463770656e248c3c0af7f38c9b2a544a9b554bb
Chenx Dust <chenx_dust@outlook.com>
2026-07-03 16:28:17 +0800

    Fix info plist
    Fix lib bug and update dependencies
    
    Fix test
    
    Fix Android core build
    
    Fix mihomo head

--- test/setup_test.dart
+++ test/setup_test.dart
@@ -96,7 +96,7 @@ void main() {
 
       expect(args, [
         'dart-define-from-file=env.json',
-        'ipa-export-method=app-store',
+        'export-method=app-store',
       ]);
     });
 
@@ -109,7 +109,7 @@ void main() {
 
       expect(args, [
         'dart-define-from-file=env.json',
-        'ipa-export-options-plist=ios/ExportOptions.plist',
+        'export-options-plist=ios/ExportOptions.plist',
       ]);
     });
   });
