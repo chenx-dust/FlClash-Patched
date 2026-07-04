f4fee47d56c028cc688c4fa5ef0c3f50a78dcfb8
Chenx Dust <chenx_dust@outlook.com>
2026-07-04 11:06:43 +0800

    Improve copy experience

--- lib/providers/action.dart
+++ lib/providers/action.dart
@@ -342,7 +342,7 @@ class SetupAction extends _$SetupAction {
       );
       return res.a;
     } catch (e) {
-      globalState.showNotifier(e.toString());
+      globalState.showNotifier(e.toString(), allowCopy: true);
     }
     return '';
   }
@@ -506,7 +506,7 @@ class CoreAction extends _$CoreAction {
     final String message = result[0];
     if (message.isNotEmpty) {
       ref.read(coreStatusProvider.notifier).value = CoreStatus.disconnected;
-      globalState.showNotifier(message);
+      globalState.showNotifier(message, allowCopy: true);
       return;
     }
     ref.read(coreStatusProvider.notifier).value = CoreStatus.connected;
