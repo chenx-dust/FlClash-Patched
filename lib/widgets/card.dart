d97ad6a8e0a0885de09ed8c58b9982609fbe754f
chen08209 <chen08209@gmail.com>
2026-07-11 19:23:48 +0800

    Adjust android process
    Optimize core service
    
    Optimize focus control
    
    Optimize app layout
    
    Fix some issues
    
    Optimize more details

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -96,7 +96,7 @@ class CommonCard extends StatelessWidget {
     this.onLongPress,
     this.shape,
     this.isError = false,
-    this.enterActionsOnRight = false,
+    this.focusNode,
     required this.child,
   }) : isSelected = isSelected ?? false;
 
@@ -113,6 +113,7 @@ class CommonCard extends StatelessWidget {
   final CommonCardType type;
   final double? radius;
   final OutlinedBorder? shape;
+  final FocusNode? focusNode;
 
   BorderSide _buildBorderSide(BuildContext context, Set<WidgetState> states) {
     final colorScheme = context.colorScheme;
@@ -227,6 +228,7 @@ class CommonCard extends StatelessWidget {
 
     final button = switch (type == CommonCardType.filled) {
       true => FilledButton(
+        focusNode: focusNode,
         onLongPress: onLongPress,
         clipBehavior: Clip.antiAlias,
         style:
@@ -254,6 +256,7 @@ class CommonCard extends StatelessWidget {
         child: childWidget,
       ),
       false => OutlinedButton(
+        focusNode: focusNode,
         onLongPress: onLongPress,
         clipBehavior: Clip.antiAlias,
         style:
