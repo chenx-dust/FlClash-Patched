da4c9d94c870a4260c4231535a96c904eca5ddff
Chenx Dust <chenx_dust@outlook.com>
2026-07-13 14:54:16 +0800

    Normalize English and Russian label casing

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -739,14 +739,14 @@ class AppLocalizations {
     );
   }
 
-  /// `Theme mode`
+  /// `Theme Mode`
   String get themeMode {
-    return Intl.message('Theme mode', name: 'themeMode', desc: '', args: []);
+    return Intl.message('Theme Mode', name: 'themeMode', desc: '', args: []);
   }
 
-  /// `Theme color`
+  /// `Theme Color`
   String get themeColor {
-    return Intl.message('Theme color', name: 'themeColor', desc: '', args: []);
+    return Intl.message('Theme Color', name: 'themeColor', desc: '', args: []);
   }
 
   /// `Preview`
@@ -1104,10 +1104,10 @@ class AppLocalizations {
     );
   }
 
-  /// `Go to download`
+  /// `Go to Download`
   String get goDownload {
     return Intl.message(
-      'Go to download',
+      'Go to Download',
       name: 'goDownload',
       desc: '',
       args: [],
@@ -1519,10 +1519,10 @@ class AppLocalizations {
     );
   }
 
-  /// `Monochrome tray icon`
+  /// `Monochrome Tray Icon`
   String get monochromeTrayIcon {
     return Intl.message(
-      'Monochrome tray icon',
+      'Monochrome Tray Icon',
       name: 'monochromeTrayIcon',
       desc: '',
       args: [],
@@ -3289,10 +3289,10 @@ class AppLocalizations {
     return Intl.message('Just now', name: 'justNow', desc: '', args: []);
   }
 
-  /// `Don't remind again`
+  /// `Don't Remind Again`
   String get noLongerRemind {
     return Intl.message(
-      'Don\'t remind again',
+      'Don\'t Remind Again',
       name: 'noLongerRemind',
       desc: '',
       args: [],
