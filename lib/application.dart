00c508679afdaa58401cf6e28115f776205c8361
Chenx Dust <chenx_dust@outlook.com>
2026-07-17 10:54:12 +0800

    Prevent unnecessary ip check

--- lib/application.dart
+++ lib/application.dart
@@ -121,8 +121,10 @@ class ApplicationState extends ConsumerState<Application> {
             commonPrint.log('connectivityChanged ${results.toString()}');
             ref.read(systemActionProvider.notifier).updateLocalIp();
             final hasVpn = results.contains(ConnectivityResult.vpn);
-            if (_preHasVpn == hasVpn) {
-              ref.read(checkIpNumProvider.notifier).add();
+            if (_preHasVpn == hasVpn && !hasVpn) {
+              ref
+                  .read(networkDetectionProvider.notifier)
+                  .startCheck(force: true);
             }
             _preHasVpn = hasVpn;
           },
