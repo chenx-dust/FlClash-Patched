70d06fa8fe050879978d6d25998d449d1e4ed12d
Chenx Dust <chenx_dust@outlook.com>
2026-07-21 00:34:45 +0800

    Add IPC peer process authorization

--- lib/core/service.dart
+++ lib/core/service.dart
@@ -136,6 +136,7 @@ class CoreService extends CoreHandlerInterface {
     if (_process != null) {
       await shutdown(false);
     }
+    await _transport.clearPeerAuthorization();
     final nextConnection = _transport.waitForNextConnection();
     if (system.isWindows && await system.checkIsAdmin()) {
       final processId = await request.startCoreByHelper(_transport.address);
@@ -152,7 +153,10 @@ class CoreService extends CoreHandlerInterface {
     }
     try {
       _process = await Process.start(appPath.corePath, [_transport.address]);
+      await _transport.authorizePeer(_process!.pid);
     } catch (e) {
+      _process?.kill();
+      _process = null;
       commonPrint.log(
         'Failed to start core process: $e',
         logLevel: LogLevel.error,
@@ -212,6 +216,7 @@ class CoreService extends CoreHandlerInterface {
   @override
   Future<bool> shutdown(bool isUser) async {
     _shutdownCompleter = Completer();
+    await _transport.clearPeerAuthorization();
     if (system.isWindows) {
       await request.stopCoreByHelper();
     }
