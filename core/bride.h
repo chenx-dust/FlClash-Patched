1c85c65b25f0022f181ce313a2363e59110724e7
Chenx Dust <chenx_dust@outlook.com>
2026-07-21 18:36:02 +0800

    Improve memory safety and usage

--- core/bride.h
+++ core/bride.h
@@ -4,6 +4,8 @@
 
 extern void (*release_object_func)(void *obj);
 
+extern void *(*retain_object_func)(void *obj);
+
 extern void (*free_string_func)(char *data);
 
 extern void (*protect_func)(void *tun_interface, int fd);
@@ -19,6 +21,8 @@ extern void protect(void *tun_interface, int fd);
 extern char* resolve_process(void *tun_interface, int protocol, const char *source, const char *target, int uid);
 
 extern void release_object(void *obj);
+
+extern void *retain_object(void *obj);
 
 extern void free_string(char *data);
 
