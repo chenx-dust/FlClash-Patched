1b167467016b60aa4f55746bf93f5dc1a3984a3e
Chenx Dust <chenx_dust@outlook.com>
2026-07-21 17:40:36 +0800

    Refactor network detection

--- lib/providers/app.dart
+++ lib/providers/app.dart
@@ -414,7 +414,6 @@ class NetworkDetection extends _$NetworkDetection
     with AutoDisposeNotifierMixin {
   static const _timeoutDisplayDelay = Duration(seconds: 2);
 
-  bool? _preIsStart;
   CancelToken? _cancelToken;
   Timer? _timeoutTimer;
   int _checkVersion = 0;
@@ -438,15 +437,10 @@ class NetworkDetection extends _$NetworkDetection
     if (!isInit) {
       return;
     }
-    final isStart = ref.read(isStartProvider);
-    if (!isStart && _preIsStart == false && state.ipInfo != null) {
-      return;
-    }
     final cancelToken = CancelToken();
     final version = _resetCheckSession(cancelToken);
     commonPrint.log('checkIp start');
     state = state.copyWith(isLoading: true, ipInfo: null);
-    _preIsStart = isStart;
     final res = await request.checkIp(cancelToken: cancelToken);
     commonPrint.log('checkIp res: $res');
 
