fd9f581f852603fd6a6df16b59a77a87a9c3846b
Chenx Dust <chenx_dust@outlook.com>
2026-07-22 01:09:49 +0800

    Add signal handler

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:fl_clash/pages/error.dart';
+import 'package:fl_clash/providers/providers.dart';
 import 'package:fl_clash/state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -18,6 +19,18 @@ Future<void> main() async {
     }
     final version = await system.version;
     final container = await globalState.init(version);
+    if (system.isDesktop) {
+      final signals = [
+        ProcessSignal.sigint,
+        if (!system.isWindows) ProcessSignal.sigterm,
+      ];
+      for (final signal in signals) {
+        signal.watch().listen((signal) {
+          commonPrint.log('Received process signal: ${signal.name}');
+          unawaited(container.read(systemActionProvider.notifier).handleExit());
+        });
+      }
+    }
     HttpOverrides.global = FlClashHttpOverrides();
     runApp(
       UncontrolledProviderScope(
