40aa506b0c422d51d3f73476f06554df55fdd55d
Chenx Dust <chenx_dust@outlook.com>
2026-08-01 15:33:29 +0800

    Show profile download network failure details

--- lib/common/request.dart
+++ lib/common/request.dart
@@ -23,6 +23,8 @@ class Request {
     _clashDio.httpClientAdapter = IOHttpClientAdapter(
       createHttpClient: () {
         final client = HttpClient();
+        client.badCertificateCallback =
+            FlClashHttpOverrides.handleBadCertificate;
         client.findProxy = (Uri uri) {
           client.userAgent = globalState.ua;
           return FlClashHttpOverrides.handleFindProxy(uri);
@@ -44,6 +46,10 @@ class Request {
       commonPrint.log('getFileResponseForUrl error ${e.toString()}');
       if (e is DioException) {
         if (e.type == DioExceptionType.unknown) {
+          final detail = e.error?.toString().trim();
+          if (detail != null && detail.isNotEmpty) {
+            throw '${currentAppLocalizations.unknownNetworkError}\n$detail';
+          }
           throw currentAppLocalizations.unknownNetworkError;
         } else if (e.type == DioExceptionType.badResponse) {
           final response = e.response;
@@ -56,7 +62,7 @@ class Request {
         }
         rethrow;
       }
-      throw currentAppLocalizations.unknownNetworkError;
+      throw '${currentAppLocalizations.unknownNetworkError}\n$e';
     }
   }
 
