1e37b5eb6881d761a1e3dc02cab3c2bfaec5ca75
Chenx Dust <chenx_dust@outlook.com>
2026-08-02 17:59:38 +0800

    Refine dashboard status and network feedback

--- lib/providers/app.dart
+++ lib/providers/app.dart
@@ -344,7 +344,6 @@ class NetworkDetection extends _$NetworkDetection
     with AutoDisposeNotifierMixin {
   static const _timeoutDisplayDelay = Duration(seconds: 2);
 
-  bool? _preIsStart;
   CancelToken? _cancelToken;
   Timer? _timeoutTimer;
   int _checkVersion = 0;
@@ -363,20 +362,19 @@ class NetworkDetection extends _$NetworkDetection
     }, duration: commonDuration);
   }
 
+  void toggleIpVisibility() {
+    state = state.copyWith(isIpVisible: !state.isIpVisible);
+  }
+
   Future<void> _checkIp() async {
     final isInit = ref.read(initProvider);
     if (!isInit) {
-      return;
-    }
-    final isStart = ref.read(isStartProvider);
-    if (!isStart && _preIsStart == false && state.ipInfo != null) {
       return;
     }
     final cancelToken = CancelToken();
     final version = _resetCheckSession(cancelToken);
     commonPrint.log('checkIp start');
     state = state.copyWith(isLoading: true, ipInfo: null);
-    _preIsStart = isStart;
     final res = await request.checkIp(cancelToken: cancelToken);
     commonPrint.log('checkIp res: $res');
 
