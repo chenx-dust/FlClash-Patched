5d4d96da74d04c8ee78320014473fffdd11e7c5e
Chenx Dust <chenx_dust@outlook.com>
2026-08-02 18:00:23 +0800

    Refine logs and connection browsing

--- lib/views/proxies/proxies.dart
+++ lib/views/proxies/proxies.dart
@@ -95,6 +95,10 @@ class _ProxiesViewState extends ConsumerState<ProxiesView> {
     ref.read(queryProvider(QueryTag.proxies).notifier).value = value;
   }
 
+  void _onRegexSearchChange(bool value) {
+    ref.read(searchUseRegexProvider(QueryTag.proxies).notifier).value = value;
+  }
+
   @override
   void initState() {
     super.initState();
@@ -129,13 +133,18 @@ class _ProxiesViewState extends ConsumerState<ProxiesView> {
       proxiesStyleSettingProvider.select((state) => state.type),
     );
     final isLoading = ref.watch(loadingProvider(LoadingTag.proxies));
+    final useRegex = ref.watch(searchUseRegexProvider(QueryTag.proxies));
     return CommonScaffold(
       isLoading: isLoading,
       resizeToAvoidBottomInset: false,
       floatingActionButton: _buildFAB(),
       actions: _buildActions(context),
       title: context.appLocalizations.proxies,
-      searchState: AppBarSearchState(onSearch: _onSearch),
+      searchState: AppBarSearchState(
+        onSearch: _onSearch,
+        onRegexChange: _onRegexSearchChange,
+        useRegex: useRegex,
+      ),
       body: switch (proxiesType) {
         ProxiesType.tab => ProxiesTabView(key: _proxiesTabKey),
         ProxiesType.list => const ProxiesListView(),
