9706f0ce540a09820a03d43a6b3c243119d37ec8
Chenx Dust <chenx_dust@outlook.com>
2026-08-29 04:17:20 +0800

    Handle desktop termination signals

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/pages/error.dart';
+import 'package:fl_clash/providers/providers.dart';
 import 'package:material_ui/material_ui.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rust_api/rust_api.dart';
@@ -28,6 +29,20 @@ void main() {
         await RustLib.init();
         final version = await system.init();
         final container = await bootstrap.init(version);
+        if (system.isDesktop) {
+          final signals = [
+            ProcessSignal.sigint,
+            if (!system.isWindows) ProcessSignal.sigterm,
+          ];
+          for (final signal in signals) {
+            signal.watch().listen((signal) {
+              commonPrint.log('Received process signal: ${signal.name}');
+              unawaited(
+                container.read(systemActionProvider.notifier).handleExit(),
+              );
+            });
+          }
+        }
         HttpOverrides.global = FlClashHttpOverrides(container);
         request.attach(container.read);
         runApp(
